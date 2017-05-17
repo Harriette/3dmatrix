52ce4225a81df1e3138fcf1b75b9f0337730dd3d
Harriette <nigel.desilvafamily@gmail.com>
2017-05-17 12:57:36 -0700

    Update 1

--- ui.R
+++ ui.R
@@ -13,7 +13,7 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Edit Transition Matrix"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
